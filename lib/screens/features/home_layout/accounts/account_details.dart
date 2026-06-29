8efc47facf149904a4ed85456287757d28095042
MohamedRomana <MohamedRomana124@gmail.com>
2026-06-29 21:40:19 +0300

    edit account icon error

--- lib/screens/features/home_layout/accounts/account_details.dart
+++ lib/screens/features/home_layout/accounts/account_details.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/data/models/account_model.dart';
 import '../../../../core/service/cubit/app_cubit.dart';
 import '../../../../core/theme/app_theme.dart';
 import '../../../../core/utils/money.dart';
